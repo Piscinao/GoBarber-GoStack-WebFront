33ab8161f91ed670786b00e47b1c833aaeb6f467
Piscinao <henhvj@gmail.com>
2020-10-19 22:23:59 -0300

    Token LocalStorage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,17 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/api';
 
+interface AuthState {
+  token: string;
+  // Sem mostrar todas as proriedades
+  user: object;
+}
 interface SignInCredentials {
   email: string;
   password: string;
 }
 interface AuthContextData {
-  name: string;
+  user: object;
   // transforma o método em async ele está obrigariamente retornando uma promise
   signIn(credentials: SignInCredentials): Promise<void>;
 }
@@ -15,17 +20,39 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 // componente para ser importado como contexto
 const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    // inicializa a variavel com a função, busca basados nos dado do LS
+    // preenche o estado com o token do usuário ou vazio
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    // se existe a informação de token e usuario retorna a informação
+    // parse transforma de volta s tring em um objeto
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    // forçar a tipagem do objeto
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    // logo após login preenche o estado com as informações
+    // enquanto o app estiver funcionando
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Henrique', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {/* tudo o que o context provider recebe como filho irá estar repassando */}
       {children}
     </AuthContext.Provider>
