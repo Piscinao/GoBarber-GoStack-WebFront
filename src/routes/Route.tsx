08c326464ea22e5ad57396cca57bd343ff914e4f
Piscinao <henhvj@gmail.com>
2020-10-21 22:03:28 -0300

    Rotas com validacao de autenticacao

---
+++ src/routes/Route.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Redirect,
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+} from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+// rota privada e autenticado, deu boa
+// rota privada nao estar, nao deu boa
+// rota n privada e estar autenticado, redirecionar para o dash
+// rota n ser privada enao estar, redireciona login
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Route';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exac component={SignUp} />
+    <Route path="/signup" component={SignUp} />
+
+    {/* // isPrivate e o usuario n estiver logado manda direto pra pagina de login */}
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
